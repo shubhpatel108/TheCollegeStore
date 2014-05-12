bba6973632cc846eeb6d89cff2de15e1939ff8a4
Shubham Patel <shubhpatel108@gmail.com>
2014-05-13 04:50:01 +0530

    Install devise and create User Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 TheCollegeStore::Application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
