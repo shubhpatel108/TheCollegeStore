4e83b4c25db422852e9db3c7309aec76d3eaf972
Shubham Patel <shubhpatel108@gmail.com>
2014-05-13 03:26:27 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheCollegeStore::Application.config.secret_token = 'ac86586b22a25f602ebc94df28224cc92135032ac742366e9ad26a9e4384d2e26ab9b5cd738d120f3a5485298a18cd3ceea2b6f0f99a84494f8ef6be27f7eaaa'
