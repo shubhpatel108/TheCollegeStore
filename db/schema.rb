bab99436358b8135e248525f9436cc97b8aa2446
Shubham Patel <shubhpatel108@gmail.com>
2014-05-19 00:59:45 +0530

    Allow user to register only after email confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140514185545) do
+ActiveRecord::Schema.define(:version => 20140518191609) do
 
   create_table "books", :force => true do |t|
     t.string   "title",      :null => false
@@ -54,9 +54,13 @@ ActiveRecord::Schema.define(:version => 20140514185545) do
     t.float    "authid"
     t.string   "mobile",                 :limit => 20
     t.integer  "college_id"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
   add_index "users", ["college_id"], :name => "index_users_on_college_id"
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
