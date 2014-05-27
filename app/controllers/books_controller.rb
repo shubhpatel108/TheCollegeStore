e2320f8c04c476f56f479607797ea6a9b151b332
valayvaidya28 <valayvaidya28@gmail.com>
2014-05-27 13:27:07 +0530

    Implement CRUD methods for Books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,32 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+  end
 
-    respond_to do |format|
-      format.html
-      format.json { render json: @thoughts }
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(params[:book])
+    if @book.save
+      flash[:success] = "Book is saved for selling purpose."
+      redirect_to index_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update_attributes(params[:book])
+      redirect_to :root
+    else
+      render "edit"
     end
   end
 
