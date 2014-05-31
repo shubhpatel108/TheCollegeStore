b59bd127e32c46d68f9ca31813ce7260d1aff0b3
Shubham Patel <shubhpatel108@gmail.com>
2014-05-31 06:14:12 +0530

    Remove validations for old attributes of Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,5 @@
 class Book < ActiveRecord::Base
   attr_accessible :edition, :isbn
-  validates :title, presence: true
-  validates :author, presence: true
   validates :edition, presence: true
 
   belongs_to :user
