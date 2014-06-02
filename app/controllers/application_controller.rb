99652000567a33379ecbb18502c9fbf83ac31299
Shubham Patel <shubhpatel108@gmail.com>
2014-06-02 19:20:10 +0530

    Redifine logic for checking and updatingcookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
 	def select_college
-		@college_id = params[:college_id]
-		cookies[:college_id] = @college_id
-		$college_name = College.where(:id => cookies[:college_id].to_s).first
-	    $book_names = BookGroup.all.map(&:title)
+		c_id = params[:college_id]
+		c_name = College.where(:id => c_id).first.name
+		cookies[:college_id] = c_id
+		cookies[:college_name] = c_name
+    $book_names = BookGroup.all.map(&:title)
   	redirect_to :controller => "books", :action => "index"
 	end
 
 	def check_cookies
-		if cookies[:college_id].nil?
-			@colleges = College.all
-			render :partial => 'shared/person_category_form'
+		if current_user.nil? and cookies[:college_id].nil?
+				@colleges = College.all
+				render :partial => 'shared/person_category_form'
+		elsif not current_user.nil?
+			redirect_to :action => 'select_college', :college_id => current_user.college_id
 		else
-			$college_name = College.where(:id => cookies[:college_id].to_s).first
 			redirect_to :controller => "books", :action => "index"
 		end
 	end
