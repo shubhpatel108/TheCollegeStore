1dc7056d89224ce7c584b1045c24019949f39617
Shubham Patel <shubhpatel108@gmail.com>
2014-06-03 17:22:40 +0530

    Add logic for Books being sold on site(reserved), addition/deletion to cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,11 +1,19 @@
 class CartController < ApplicationController
   def add_item
     book_id = params[:id]
-    session[:cart] ||= []
-    session[:cart] << book_id
-    respond_to do |format|
-      format.js
+    book = Book.where(:id => book_id).first
+    if book.reserved
+      @message = "Oops! Someone recently purchased this Book before you!"
+    else
+      session[:cart] ||= []
+      session[:cart] << book_id
+      @message = "Your book has been successfully added to the cart!"
+      book.reserved = true
+      book.save!
     end
+      respond_to do |format|
+        format.js
+      end
   end
 
   def show_cart
@@ -20,6 +28,9 @@ class CartController < ApplicationController
 
   def remove_item
     @book_id = params[:id]
+    book = Book.where(:id => @book_id).first
+    book.reserved = false
+    book.save!
     session[:cart].delete(@book_id)
     respond_to do |format|
       format.js
