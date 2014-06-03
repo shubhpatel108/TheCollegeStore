053a031204e590074a1994cb5a559db184ad8bf1
Shubham Patel <shubhpatel108@gmail.com>
2014-06-03 17:18:27 +0530

    Add sold and reserved status to Book Table

---
+++ db/migrate/20140603113153_add_reserved_to_books.rb
@@ -0,0 +1,9 @@
+class AddReservedToBooks < ActiveRecord::Migration
+  def up
+  	add_column :books, :reserved, :boolean, default: false
+  end
+
+  def down
+  	remove_column :books, :reserved
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140531030804) do
+ActiveRecord::Schema.define(:version => 20140603113153) do
 
   create_table "book_groups", :force => true do |t|
     t.string   "title",      :null => false
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(:version => 20140531030804) do
     t.integer  "college_id"
     t.integer  "price"
     t.integer  "book_group_id"
+    t.boolean  "sold",          :default => false
+    t.boolean  "reserved",      :default => false
   end
 
   add_index "books", ["book_group_id"], :name => "index_books_on_book_group_id"
