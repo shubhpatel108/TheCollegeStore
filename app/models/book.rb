27669409aaceec1bf3f45a011ce8936c54526d9c
prashantvithani <prashantvithani@gmail.com>
2014-06-04 18:57:27 +0530

    Add relation between book and admin_user.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-  attr_accessible :edition, :isbn, :price
+  attr_accessible :edition, :isbn, :price, :user_id, :college_id, :book_group_id
   validates :price, presence: true, :numericality => true
   validates :edition, :numericality => true
   attr_accessible :reserved, :sold
@@ -7,4 +7,7 @@ class Book < ActiveRecord::Base
   belongs_to :user
   belongs_to :college
   belongs_to :book_group
+  belongs_to :admin_user
+
+  scope :unsold, where(:reserved => false)
 end
