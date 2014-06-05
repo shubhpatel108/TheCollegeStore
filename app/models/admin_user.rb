dc8886f93b881b6d9910fae596214d5a2da60caa
prashantvithani <prashantvithani@gmail.com>
2014-06-05 20:53:46 +0530

    Add column 'full_name' in 'admin_user' table.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -5,7 +5,7 @@ class AdminUser < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :full_name
   # attr_accessible :title, :body
 
   has_many :books
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140604134327) do
+ActiveRecord::Schema.define(:version => 20140604142315) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20140604134327) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "full_name"
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
