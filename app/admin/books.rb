a7904484204943eb855f5a875994933e8b0ac4ee
prashantvithani <prashantvithani@gmail.com>
2014-06-20 16:30:07 +0530

    Customize form for books entry.

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -8,8 +8,23 @@ ActiveAdmin.register Book do
 		column :edition
 		column :isbn
 		column :price
+		column :user
 		column :sold
 		column :reserved
 		default_actions
 	end
+
+	form do |f|                         
+    f.inputs "User Details" do
+		f.input :user
+		f.input :book_group
+		f.input :college
+		f.input :edition
+		f.input :isbn
+		f.input :price               
+		f.input :sold
+		f.input :reserved  
+    end                               
+    f.actions                         
+  end     
 end
