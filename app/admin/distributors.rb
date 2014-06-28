1493261feba939d9b087cf3c19cabf7e6d35480f
prashantvithani <prashantvithani@gmail.com>
2014-06-29 00:49:07 +0530

    Add coupons and distribution tables to admin page.

---
+++ app/admin/distributors.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register Distributor do
+  
+end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,5 +1,5 @@
 class Coupon < ActiveRecord::Base
-  attr_accessible :code, :distributed
+  attr_accessible :code, :distributed, :distributor_id
   has_and_belongs_to_many :users
   belongs_to :distributor
 end
