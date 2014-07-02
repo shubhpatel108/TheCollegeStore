daa0b3677e5aa1d40f2faff6bf9ae23e377b16a7
Shubham Patel <shubhpatel108@gmail.com>
2014-07-02 13:47:26 +0530

    Add Sessions table to store it in backend

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140626152754) do
+ActiveRecord::Schema.define(:version => 20140702064539) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -113,12 +113,10 @@ ActiveRecord::Schema.define(:version => 20140626152754) do
     t.boolean  "distributed",    :default => false
     t.datetime "created_at",                        :null => false
     t.datetime "updated_at",                        :null => false
-    t.integer  "user_id"
     t.integer  "distributor_id"
   end
 
   add_index "coupons", ["distributor_id"], :name => "index_coupons_on_distributor_id"
-  add_index "coupons", ["user_id"], :name => "index_coupons_on_user_id"
 
   create_table "coupons_users", :force => true do |t|
     t.integer "coupon_id", :null => false
@@ -143,6 +141,16 @@ ActiveRecord::Schema.define(:version => 20140626152754) do
     t.datetime "created_at",                               :null => false
     t.datetime "updated_at",                               :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "first_name",                           :default => "", :null => false
