34afd25b5dea870a49ac27fb8740a8de35ce797c
Shubham Patel <shubhpatel108@gmail.com>
2014-07-17 19:30:31 +0530

    Variable superscript according to different edition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+	def superscript(number)
+		number %= 10
+		case number
+		when 1
+			return "st"
+		when 2
+			return "nd"
+		when 3
+			return "rd"
+		else
+			return "th"
+		end
+	end
 end
