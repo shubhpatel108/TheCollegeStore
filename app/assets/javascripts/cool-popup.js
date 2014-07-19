37a977fc4abbd0ba4aa0170aaa4db81517b9867a
Shreyans Gandhi <spgandhi@live.com>
2014-07-19 15:44:49 +0530

    Back To Top Button, PopUp EaseIn

--- app/assets/javascripts/cool-popup.js
+++ app/assets/javascripts/cool-popup.js
@@ -111,14 +111,3 @@
       }
     });
 
-
-    $.extend($.easing, {
-      easeOutBack: function (x, t, b, c, d, s) {
-        if (s == undefined) s = 1.70158;
-        return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
-      },
-      easeInBack: function (x, t, b, c, d, s) {
-        if (s == undefined) s = 1.70158;
-        return c*(t/=d)*t*((s+1)*t - s) + b;
-      }
-    });
