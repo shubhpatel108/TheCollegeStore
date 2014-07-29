d66d092a5a74837c6c2fb727b3037d79e501f4de
Shubham Patel <shubhpatel108@gmail.com>
2014-07-30 01:16:28 +0530

    Show flash when trying to add to wishlist and not signin

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,11 +1,19 @@
 class WishlistsController < ApplicationController
-	before_filter :authenticate_user!
+	before_filter :authenticate_user!, :only => [:show]
 	def add
 		@bg_id = params[:id]
-		if not @bg_id.nil?
-			Wishlist.create(:book_group_id => @bg_id, :user_id => current_user.id)
+		if current_user.nil?
+			@message = "You need to sign in."
+			@m_type = "warning"
 		else
-			render :js => "FlashNotice('error', 'Can not add a Book with nil id!')"
+			if not @bg_id.nil?
+				Wishlist.create(:book_group_id => @bg_id, :user_id => current_user.id)
+				@message = "Successfully addded to your Wishlist!"
+				@m_type = "success"
+			else
+				@message = "Can not add a Book with nil id!"
+				@m_type = "error"
+			end
 		end
 		respond_to do |format|
 			format.js
