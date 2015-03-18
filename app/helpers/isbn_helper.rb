5cddc0aebf41d59e23af1baecadc7772bd37b9a1
Vaibhavi <ranihaileydesai@gmail.com>
2015-03-18 12:59:48 +0530

    added isbn numbers for the novels

--- app/helpers/isbn_helper.rb
+++ app/helpers/isbn_helper.rb
@@ -8,5 +8,37 @@ module IsbnHelper
 			nil
 		end
 	end
-
+	def get_isbn(book_group_id)
+		case book_group_id
+		when 130
+			"9780062095909"
+		else
+			nil
+		end
+	end
+	def get_isbn(book_group_id)
+		case book_group_id
+		when 131
+			"9788129135728"
+		else
+			nil
+		end
+	end
+	def get_isbn(book_group_id)
+		case book_group_id
+		when 132
+			"9780399252518"
+		else
+			nil
+		end
+	end
+	def get_isbn(book_group_id)
+		case book_group_id
+		when 133
+			"9781476731353"
+		else
+			nil
+		end
+	end
+	
 end
