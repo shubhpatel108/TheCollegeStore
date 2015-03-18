13db016b05c130ef1223bfafd0e8297feb8efe57
Vaibhavi <ranihaileydesai@gmail.com>
2015-03-18 13:08:07 +0530

    corrected the syntax in isbn helper

--- app/helpers/isbn_helper.rb
+++ app/helpers/isbn_helper.rb
@@ -4,36 +4,12 @@ module IsbnHelper
 		case book_group_id
 		when 88
 			"9780201662108"
-		else
-			nil
-		end
-	end
-	def get_isbn(book_group_id)
-		case book_group_id
 		when 130
 			"9780062095909"
-		else
-			nil
-		end
-	end
-	def get_isbn(book_group_id)
-		case book_group_id
 		when 131
 			"9788129135728"
-		else
-			nil
-		end
-	end
-	def get_isbn(book_group_id)
-		case book_group_id
 		when 132
 			"9780399252518"
-		else
-			nil
-		end
-	end
-	def get_isbn(book_group_id)
-		case book_group_id
 		when 133
 			"9781476731353"
 		else
